c8f1732474a76c807bc0bea5867e958c13e5c38a
sravan <sravansangepu18@hotmail.com>
2025-05-12 05:13:33 +0100

    adding new assertions for dates

--- cypress/e2e/cucumber/stepDefinitions/trainTicketBooking/trainTicketBooking.js
+++ cypress/e2e/cucumber/stepDefinitions/trainTicketBooking/trainTicketBooking.js
@@ -1,10 +1,8 @@
 /// <reference types="cypress" />
 
 import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps'
-//import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor';
 import basepage from '../../../pages/basepage';
 import buytrainticketspage from '../../../pages/buytrainticketspage';
-//import onlineticketofficepage from '../../../pages/onlineticketofficepage';
 
 const date = new Date()
 const day = date.getDate();
@@ -25,10 +23,8 @@ Given(/^User is on the tickets page$/, () => {
 
 Given(/^User is on the buy-tickets page$/, function () {
 
-    //cy.visit('https://www.cp.pt/passageiros/en/buy-tickets')
-    //basepage.navigateToUrl('https://www.cp.pt/passageiros/en/buy-tickets')
     basepage.navigateToUrl(Cypress.env('url')+'passageiros/en/buy-tickets')
-    //Cypress.config('defaultCommandTimeout', 6000)
+
 });
 
 When(/^User enters From and To details$/, function (dataTable) {
@@ -48,19 +44,12 @@ When(/^User enters on date and return date$/, function () {
 
 When(/^User clicks on the Submit$/, function () {
      buytrainticketspage.clickSubmitButton();
-    //onlineticketofficePage.openDepartCalendar();
 
 });
 
 Then(/^User navigates to Online-Ticket-Office page$/, function () {
-    // const clickCancelBtn = onlineticketofficepage.clickCancelButton();
-    // cy.origin('https://venda.cp.pt/', { args: { clickCancelBtn } }, ({ clickCancelBtn }) => {
 
-    //    clickCancelBtn
-    //     //cy.get('#exitButton').click()
-    // })
     cy.origin('https://venda.cp.pt/',  ()=> {
-        //cy.get('#exitButton').click()
         const {OnlineTicketOfficePage} = Cypress.require('../../../pages/onlineticketofficepage')
         const onlineticketofficepage = new OnlineTicketOfficePage()
         onlineticketofficepage.clickCancelButton()
@@ -70,11 +59,14 @@ Then(/^User navigates to Online-Ticket-Office page$/, function () {
 
 
 Then(/^User entered journey details for From and To is correct$/, function() {
-    //cy.get("input[placeholder='From ']").should('have.value', this.data.From)
+
+    var day3 = Number(`${day}`) + 3;
+    var day5 = Number(`${day}`) + 5;
     cy.get(buytrainticketspage.getFromField()).should('have.value', this.data.From)
     cy.get(buytrainticketspage.getToField()).should('have.value', this.data.To)
-    //buytrainticketspage.getFromField(this.data.From)
-    //buytrainticketspage.enteredToField(this.data.To)
+    cy.get('input[name="departDate"]').should('have.value', `${day3}`+" May, 2025")
+    cy.get('input[name="returnDate"]').should('have.value', `${day5}`+" May, 2025")
+
 	
 });
 
